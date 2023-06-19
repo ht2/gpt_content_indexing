7867c3b458a3225b89d0aa13b033df585522ca46
James Mullaney <james.mullaney@learningpool.com>
2023-06-19 16:44:15 +0100

    incl description

--- atlas_visualise.py
+++ atlas_visualise.py
@@ -4,6 +4,9 @@ import numpy as np
 from nomic import atlas
 import os
 import pandas as pd
+import openai
+from datetime import datetime
+
 from pprint import pprint
 
 # Create an ArgumentParser object
@@ -11,6 +14,7 @@ parser = argparse.ArgumentParser()
 parser.add_argument("--index", default="default", help="Pinecone Index")
 parser.add_argument("--namespace", default=False, help="Pinecone Namespace")
 parser.add_argument("--file", default="./output/default/contents.csv", help="Specify the path to the CSV")
+parser.add_argument("--lines", type=int, help="Number of lines to read from the CSV")
 args = parser.parse_args()
 
 PINECONE_REGION = "us-east1-gcp"
@@ -30,25 +34,46 @@ if args.namespace:
     query_params["namespace"] = args.namespace
 
 # Load IDs from the CSV file
-df = pd.read_csv(args.file)
-all_ids = df.iloc[:, 0].tolist()
+df = pd.read_csv(args.file, nrows=args.lines)
+all_data = df.to_dict('records')
+
+# Create a function to generate a brief description
+def get_brief_description(text):
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo-0613",
+        messages=[
+            {"role": "user", "content": text},
+            {"role": "system", "content": "Describe this training in two to five words, followed by the URL to the content:"}
+        ],
+        temperature=0.5,
+        max_tokens=100
+    )
+
+    return response['choices'][0]['message']['content'].strip()
 
 ids = []
 embeddings = []
+descriptions = []
 
 # Fetch vectors for the IDs in batches of 100
-for i in range(0, len(all_ids), 100):
-    pprint("Fetching batch of IDs from {} to {}".format(i, i+100))
-    batch_ids = all_ids[i:i+100]
-    result = index.fetch(ids=batch_ids, **query_params)
-    for id, vector in result['vectors'].items():
-        ids.append(id)
-        embeddings.append(vector['values'])
+for i in range(0, len(all_data), 100):
+    pprint(f"{datetime.now().strftime('%H:%M:%S')} - Fetching batch of IDs from {i} to {i+100}")
+    batch_data = all_data[i:i+100]
+    for d in batch_data:
+        result = index.fetch(ids=[d['id']], **query_params)
+        for id, vector in result['vectors'].items():
+            ids.append(id)
+            embeddings.append(vector['values'])
+            description = get_brief_description(d['content'])
+            descriptions.append(description)
 
-pprint("Fetched {} vectors".format(len(embeddings)))
+pprint(f"{datetime.now().strftime('%H:%M:%S')} - Fetched {len(embeddings)} vectors")
 
 embeddings = np.array(embeddings)
 
-pprint("Pushing embeddings to Atlas")
-atlas.map_embeddings(embeddings=embeddings, data=[{'id': id} for id in ids], id_field='id')
-pprint("Done")
+# prepare data for atlas.map_embeddings
+data = [{'id': id, 'description': desc} for id, desc in zip(ids, descriptions)]
+
+pprint(f"{datetime.now().strftime('%H:%M:%S')} - Pushing embeddings to Atlas")
+atlas.map_embeddings(embeddings=embeddings, data=data, id_field='id')
+pprint(f"{datetime.now().strftime('%H:%M:%S')} - Done")
